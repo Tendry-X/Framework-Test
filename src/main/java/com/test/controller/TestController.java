0bc653b41ca2aa61c43a9cc774565f3b8b05f3d8
Tendry-X <tandriabarimanana@gmail.com>
2024-10-18 18:00:00 +0300

    Sprint 2: Contrôleur avec annotations @MyAnnotation

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,9 +1,23 @@
 package com.test.controller;
 
+import mg.itu.framework.annotation.MyAnnotation;
+import mg.itu.framework.annotation.HttpMethod;
+
 /**
- * SPRINT 1 : Test simple
- * Le framework intercepte juste l'URL
+ * SPRINT 2 : Premier contrôleur avec annotations
  */
+@MyAnnotation(value = "", method = HttpMethod.GET)
 public class TestController {
-    // Pas encore d'annotations - Sprint 1 n'a que l'interception d'URL
+    
+    @MyAnnotation(value = "/hello", method = HttpMethod.GET)
+    public String hello() {
+        return "<h1>Hello from Sprint 2!</h1>" +
+               "<p>Le framework supporte maintenant les annotations @MyAnnotation</p>";
+    }
+    
+    @MyAnnotation(value = "/", method = HttpMethod.GET)
+    public String home() {
+        return "<h1>Framework-Test - Sprint 2</h1>" +
+               "<p>Essayez : <a href='/framework-test/hello'>/hello</a></p>";
+    }
 }
