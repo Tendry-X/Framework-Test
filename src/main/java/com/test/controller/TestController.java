5cc7f9817e013dc53ede183e7bc37adfb4f7e327
Tendry-X <tandriabarimanana@gmail.com>
2024-11-08 18:00:00 +0300

    Sprint 3bis: Paramètres dynamiques /etudiant/{id}

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -4,25 +4,32 @@ import mg.itu.framework.annotation.MyAnnotation;
 import mg.itu.framework.annotation.HttpMethod;
 import mg.itu.framework.model.ModelView;
 import com.test.model.Etudiant;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * SPRINT 3 : Utilisation de ModelView
+ * SPRINT 3bis : Paramètres dynamiques {id}
  */
 @MyAnnotation(value = "", method = HttpMethod.GET)
 public class TestController {
     
-    @MyAnnotation(value = "/hello", method = HttpMethod.GET)
-    public String hello() {
-        return "<h1>Hello from Sprint 3!</h1>";
+    private static List<Etudiant> etudiants = new ArrayList<>();
+    
+    static {
+        etudiants.add(new Etudiant("Rakoto", "Jean"));
+        etudiants.add(new Etudiant("Rabe", "Marie"));
+        etudiants.add(new Etudiant("Rasoa", "Paul"));
     }
     
-    @MyAnnotation(value = "/etudiant", method = HttpMethod.GET)
+    @MyAnnotation(value = "/hello", method = HttpMethod.GET)
+    public String hello() {
+        return "<h1>Hello from Sprint 3bis!</h1>";
+    }
+    
+    @MyAnnotation(value = "/etudiant/{id}", method = HttpMethod.GET)
     public ModelView showEtudiant() {
         ModelView mv = new ModelView("etudiant");
-        
-        Etudiant etudiant = new Etudiant("Rakoto", "Jean");
-        mv.addItem("etudiant", etudiant);
-        
+        mv.addItem("etudiant", etudiants.get(0));
         return mv;
     }
 }
